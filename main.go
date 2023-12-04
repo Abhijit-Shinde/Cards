50816987c4b38a29a2b6ec03e4673e4976f31f5a
Abhijit Shinde <Abhijit.Shinde@blueconchtech.com>
2023-12-04 16:31:17 +0530

    file read assignment

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"io"
+	"os"
 )
 
-type shape interface {
-	getArea() float64
-}
-
-type triangle struct {
-	height float64
-	base   float64
-}
-type square struct {
-	sideLength float64
-}
-
-func (t triangle) getArea() float64 {
-	return 0.5 * t.base * t.height
-}
-
-func (s square) getArea() float64 {
-	return s.sideLength * s.sideLength
-}
-
-func printArea(s shape) {
-	fmt.Println(s.getArea())
-}
-
 func main() {
+	arguments := os.Args
 
-	printArea(triangle{base: 2, height: 3})
-	printArea(square{sideLength: 4})
-	
+	file,_ := os.Open(arguments[1])
+
+	io.Copy(os.Stdout,file)
+
 }
